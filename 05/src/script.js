9751320c1cd146c7a1b2a2ab34228a781c875766
erratinsilentio <kacper.kondracki@wp.pl>
2023-03-14 09:23:41 +0100

    completed lesson 05

---
+++ 05/src/script.js
@@ -0,0 +1,75 @@
+import * as THREE from "three";
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Objects
+ */
+const group = new THREE.Group();
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff9900 })
+);
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+cube2.position.x = -2;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff1199 })
+);
+cube3.position.x = 2;
+group.add(cube3);
+
+//position
+group.position.set(-4, 0, -5);
+//scale
+group.scale.set(2, 3.5, 0.5);
+//rotation
+group.rotation.reorder("YXZ");
+group.rotation.x = Math.PI * 3;
+group.rotation.y = Math.PI * 1.3;
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+camera.position.x = 2;
+
+scene.add(camera);
+
+//center camera on our object
+camera.lookAt(group.position);
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
